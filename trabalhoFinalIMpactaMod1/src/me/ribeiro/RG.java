10ddc5b6795d18377a7b1c10e8d7e70569afea22
vitor <v.b.ribeiro@hotmail.com>
2025-12-29 12:42:40 -0300

    "Criação classe RG"

---
+++ trabalhoFinalIMpactaMod1/src/me/ribeiro/RG.java
@@ -0,0 +1,20 @@
+package me.ribeiro;
+
+public class RG {
+	
+	private int numero;
+	private String dataNasc;
+	public int getNumero() {
+		return numero;
+	}
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+	public String getDataNasc() {
+		return dataNasc;
+	}
+	public void setDataNasc(String dataNasc) {
+		this.dataNasc = dataNasc;
+	}
+	
+}
